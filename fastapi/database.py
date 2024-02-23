818f81ed59761543cb63ee3d47e20a8b094f6c03
gomespr <prsgomes88@gmail.com>
2024-02-22 23:33:26 -0300

    docker configuration

--- fastapi/database.py
+++ fastapi/database.py
@@ -1,10 +1,17 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
+import os
 
-load_dotenv()
-DATABASE_URL = os.environ.get('DATABASE_URL')
+db_driver = os.getenv('DB_DRIVER')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+postgres_db = os.getenv('POSTGRES_DB')
+postgres_user = os.getenv('POSTGRES_USER')
+postgres_password = os.getenv('POSTGRES_PASSWORD')
+
+DATABASE_URL = f'{db_driver}://{postgres_user}:{postgres_password}@{db_host}:{db_port}/{postgres_db}'
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
